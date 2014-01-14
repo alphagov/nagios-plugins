1ec7bdc5eb816a90bf4ffb37c7e5ae73ce303702
Tom Booth <tombooth@gmail.com>
2014-01-14 14:32:45 +0000

    Pull output functions out to module
    These functions will want to be shared between any future commands so it
    seemed prudent to pull them out to a module that can be shared.

--- plugins/command/check_reboot_required.py
+++ plugins/command/check_reboot_required.py
@@ -8,37 +8,11 @@ import os
 import re
 import sys
 
-
-class CheckException(Exception):
-    def __init__(self, message, severity):
-        Exception.__init__(self, message)
-        self.severity = severity
-
-
-def nagios_message(message, exitcode):
-    """Format a Nagios message and exit"""
-    print message
-    sys.exit(exitcode)
-
-
-def nagios_ok(message):
-    """Nagios OK message"""
-    raise CheckException("OK: %s" % message, 0)
-
-
-def nagios_warning(message):
-    """Nagios WARNING message"""
-    raise CheckException("WARNING: %s" % message, 1)
-
-
-def nagios_critical(message):
-    """Nagios CRITICAL message"""
-    raise CheckException("CRITICAL: %s" % message, 2)
-
-
-def nagios_unknown(message):
-    """Nagios UNKNOWN message"""
-    raise CheckException("UNKNOWN: %s" % message, 3)
+from plugins.output import (nagios_ok,
+                            nagios_warning,
+                            nagios_critical,
+                            nagios_unknown,
+                            nagios_message)
 
 
 def dpkg_log_lines(log_files):
